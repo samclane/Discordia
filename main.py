09427d807c504c243dcdeb148a52d8b00f5170a2
Sawyer McLane <samclane@gmail.com>
2019-07-29 14:24:15 -0600

    Added argparser for showing world window (Default false)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import logging
 import os
 import pickle
 import threading
+import argparse
 
 import ConfigParser
 from Discordia.GameLogic import GameSpace
@@ -14,6 +15,12 @@ logging.basicConfig(level=logging.INFO)
 
 
 def main():
+    parser = argparse.ArgumentParser(description="Run an instance of a Discordia server",
+                                     prog="Discordia")
+    parser.add_argument('-W --show_window', dest='show_window', action='store_const', const=True, default=False,
+                        help="Show a window containing a live view of the entire world. WARNING: CPU-intensive.")
+    args = parser.parse_args()
+
     # Read in world file if found
     if os.path.isfile(r'./world.p'):
         world: GameSpace.World = pickle.load(open(r'./world.p', 'rb'))
@@ -24,7 +31,7 @@ def main():
 
     display = MainWindow(adapter)
 
-    threading.Thread(target=update_display, args=(display, True), daemon=True).start()
+    threading.Thread(target=update_display, args=(display, args.show_window), daemon=True).start()
     discord_interface = DiscordInterface(adapter)
     # discord_interface.bot.loop.create_task(update_display(display))
     # threading.Thread(target=discord_interface.bot.run, args=(ConfigParser.DISCORD_TOKEN,), daemon=True).start()
